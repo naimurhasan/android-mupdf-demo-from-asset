c7d72ae7397b5753df39e867ece9fd318919ff49
Naimur <imuhasan98@gmail.com>
2021-02-24 13:28:22 +0600

    Offline Latest Mu-pdf Workng

--- app/src/main/java/com/example/myapplication5/FirstFragment.java
+++ app/src/main/java/com/example/myapplication5/FirstFragment.java
@@ -3,14 +3,12 @@ package com.example.myapplication5;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 import com.artifex.mupdf.viewer.DocumentActivity;
 
 import java.io.File;
